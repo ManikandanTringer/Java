9c07d9414d23c534ad512d45ed664ae240fcd152
ManikandanTringer <96737827+ManikandanTringer@users.noreply.github.com>
2022-04-21 15:22:52 +0530

    Add files via upload

---
+++ Crazy8Project/src/main/java/cardgame/PlayerOne.java
@@ -0,0 +1,61 @@
+package cardgame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cardgame.Card.Suit;
+
+public class PlayerOne implements PlayerStrategy {
+
+	public int playerid;
+	public int opponentid;
+	private List<Card>player1_cards=new ArrayList<>();
+
+		@Override
+	public void init(int playerId, int opponentId) {
+			this.playerid =playerId;
+			this.opponentid =opponentId;
+	}
+
+	@Override
+	public void receiveInitialCards(List<Card> cards) {
+		player1_cards=cards;
+	}
+
+	@Override
+	public boolean shouldDrawCard(Card topPileCard, Suit changedSuit) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void receiveCard(Card drawnCard) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Card playCard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Suit declareSuit() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void processOpponentActions(List<PlayerTurn> opponentActions) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ Crazy8Project/src/main/java/cardgame/PlayerTwo.java
@@ -0,0 +1,52 @@
+package cardgame;
+
+import java.util.List;
+
+import cardgame.Card.Suit;
+
+public class PlayerTwo implements PlayerStrategy{
+
+
+	@Override
+	public void receiveInitialCards(List<Card> cards) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean shouldDrawCard(Card topPileCard, Suit changedSuit) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void receiveCard(Card drawnCard) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Card playCard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Suit declareSuit() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void processOpponentActions(List<PlayerTurn> opponentActions) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
